b802c3e76a931bb031e0dffcd33a19d98e0eca48
min99039 <minjung@finda.co.kr>
2024-07-16 21:09:53 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from openai import OpenAI
+import time
+import streamlit as st
+
+# 코드스니펫 - 제목 
+st.title('나만의 제품 홍보 만들기')
+
+# 코드스니펫 - 입력
+keyword = st.text_input("키워드를 입력하세요.")
+
+if st.button('만들기'):
+  st.write('완성!')
+  with st.spinner('Wait for it...'):
+
+
+
+     client = OpenAI(api_key=st.secrets["API_KEY"])
+
+     chat_completion = client.chat.completions.create(
+         messages=[{
+             "role": "user",
+             "content": keyword + '라는 주제로 새로운 제품을 홍보할 수 있는 카피를 40자 이내로 만들어줘',
+         }],
+         model="gpt-4o",
+     )
+
+     chat_result = chat_completion.choices[0].message.content
+    
+     st.write(chat_result)
+     client = OpenAI(api_key=st.secrets["API_KEY"])
+     response = client.images.generate(
+       model="dall-e-3",
+       prompt=keyword,
+       size="1024x1024",
+       quality="standard",
+       n=1,
+     )
+    
+     image_url = response.data[0].url
+     # 코드스니펫 - 이미지
+     st.image(image_url)
